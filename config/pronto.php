f6b03d21bdf002f5a928874908550fb454d4b2e9
alessio <alessio.vertemati@gmail.com>
2016-04-28 12:31:35 +0200

    added footer messages configuration

--- config/pronto.php
+++ config/pronto.php
@@ -36,5 +36,15 @@ return [
      * Application UI theme
      */
     'theme' => env('PRONTO_APP_THEME', 'default'),
+    
+    
+    /** 
+     * Footer configuration section
+     */
+    'footer' => [
+        'copyright_holder' => env('PRONTO_COPYRIGHT_HOLDER', 'someone'),
+        'start_year' => !!env('PRONTO_COPYRIGHT_START_YEAR', false),
+        'tag_line' => env('PRONTO_FOOTER_TAG_LINE', 'Built with <span class="love">♥</span> and <a href="https://github.com/avvertix/pronto-cms/">Pronto</a>'),
+    ]
 
 ];
